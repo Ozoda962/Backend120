22630c5d00eb010ab7f040e0d0ab40afc782f9a3
Ozoda962 <asadovaozoda9622gmail.com>
2025-07-03 19:42:45 +0500

    third messages

--- main.py
+++ main.py
@@ -5,3 +5,25 @@ b=5
 c = a + b
 
 print(c)
+
+
+from datetime import date,timedelta
+
+def yakshanbalar_royxati(yil):
+    
+    boshlanish = date(yil, 1, 1)
+    tugash = date(yil, 12, 31)
+    kun = boshlanish
+    royxat = []
+
+    while kun <= tugash:
+        if kun.weekday() == 6:
+            royxat.append(kun)
+        kun += timedelta(days=1)
+
+    for sana in royxat:
+        print(sana)
+    
+
+
+print(yakshanbalar_royxati(2024))
